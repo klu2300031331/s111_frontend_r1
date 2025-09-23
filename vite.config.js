bcacd691cc4e6619f4235a25741c972e89befc2f
Thota Gowtham <163727915+klu2300031331@users.noreply.github.com>
2025-09-23 22:14:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'   // ✅ important: use relative paths
+  base: '/frontapp1/'  // ✅ important: use relative paths
 })
